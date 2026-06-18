ece938422a0c38bbcfd015755c7bb4fd345dac8c
Levi Blaque <chinosochimuananys@gmail.com>
2026-06-18 18:16:50 +0100

    Updated the header to display content base on page

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -1,14 +1,44 @@
+import { useLocation } from 'react-router-dom'
 import './MobileHeader.css'
 
 export function MobileHeader({handleDialogOpen}) {
+  const location = useLocation();
   return (
     <header className='mobile-home-header'>
       <div className="phone-size">
-        <div className="name">Flow<span>via</span></div>
+        {location.pathname === '/' && (
+          <div className="name">Flow<span>via</span></div>
+        )}
+        {location.pathname.startsWith('/marketplace') && (
+          <div className="name">Marketplace</div>
+        )}
+        {location.pathname.startsWith('/status') && (
+          <div className="name">Status</div>
+        )}
+        {location.pathname.startsWith('/projects') && (
+          <div className="name">Projects</div>
+        )}
+        {location.pathname.startsWith('/messages') && (
+          <div className="name">Messages</div>
+        )}
+        {location.pathname.startsWith('/work-circle') && (
+          <div className="name">Work Circle</div>
+        )}
+        {location.pathname.startsWith('/notification') && (
+          <div className="name">Notification</div>
+        )}
+        {location.pathname.startsWith('/profile') && (
+          <div className="name">Profile</div>
+        )}
+        {location.pathname.startsWith('/settings') && (
+          <div className="name">Settings</div>
+        )}
         <div className="right-side">
-          <span>
-            <i className="fa-solid fa-search"></i>
-          </span>
+          {location.pathname === '/' && (
+            <span>
+              <i className="fa-solid fa-search"></i>
+            </span>
+          )}
           <span>
             <i className="fa-regular fa-bell"></i>
             <div className='notification'></div>
